b6ef7fd748591ac62f7aff71e2ef6fea82020bdb
cbl6499 <clemens.blecha@students.fhv.at>
2018-04-16 11:16:33 +0200

    Login welcome message added

--- app/src/main/java/com/example/clemensb/a177147/MainActivity.java
+++ app/src/main/java/com/example/clemensb/a177147/MainActivity.java
@@ -200,7 +200,9 @@ public class MainActivity extends AppCompatActivity {
 
         AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
 
-        Toast.makeText(MainActivity.this, "" + authCredential.getProvider(), Toast.LENGTH_LONG).show();
+        //Toast.makeText(MainActivity.this, "" + authCredential.getProvider(), Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "Welcome " + googleSignInAccount.getDisplayName(), Toast.LENGTH_LONG).show();
+
 
         firebaseAuth.signInWithCredential(authCredential)
                 .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
