d41691952db5cc3cf5f7dfa7da73efd178b4fc0f
janflei <fleisch.ja@gmail.com>
2018-04-23 12:47:41 +0200

    animation for spawn

--- app/src/main/java/com/example/clemensb/a177147/BoardActivity.java
+++ app/src/main/java/com/example/clemensb/a177147/BoardActivity.java
@@ -363,7 +363,6 @@ public class BoardActivity extends Activity {
             final Animation animation = new AlphaAnimation(1, 0); // Change alpha from fully visible to invisible
             animation.setDuration(500); // duration - half a second
             animation.setInterpolator(new LinearInterpolator()); // do not alter animation rate
-            animation.setRepeatCount(Animation.INFINITE); // Repeat animation infinitely
             animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in
 
 
