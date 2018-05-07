6bad4a290c34627d412eba3d1ab3fd0fbaac0407
ChristophRheinberger <crh6731@students.fhv.at>
2018-05-07 12:24:17 +0200

    Start App on Phone Startup fix

--- app/src/main/java/com/example/clemensb/a177147/StartAppReceiver.java
+++ app/src/main/java/com/example/clemensb/a177147/StartAppReceiver.java
@@ -1,5 +1,6 @@
 package com.example.clemensb.a177147;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,8 +13,10 @@ import android.content.pm.PackageManager;
 public class StartAppReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        PackageManager pm = context.getPackageManager();
-        Intent launchIntent = pm.getLaunchIntentForPackage("com.example.clemensb.a177147");
-        context.startActivity(launchIntent);
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
     }
 }
